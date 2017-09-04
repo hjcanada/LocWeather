120c3bf74dd95e6830ebfe9ecb172306718f5da9
root <root@localhost.localdomain>
2017-09-04 04:13:39 -0400

    Initial Commit

---
+++ public/controller/controller.js
@@ -0,0 +1,83 @@
+var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function($scope, $http) {
+	
+	$scope.wthAquire = function() {
+
+		navigator.geolocation.getCurrentPosition(function (position) {
+
+		var appid = $scope.appid;
+		var lat = position.coords.latitude;
+		var lon = position.coords.longitude;
+		var url = "/weather?lat=" + lat + "&lon=" + lon + "&appid=" + appid;
+		// debug
+		// console.log("Ctrl URL: " + url);
+
+		$http.get(url).then(function(res) {
+			if (res.data.cod == 401) {
+				$scope.value2 = "true";
+				$scope.value1 = !$scope.value2;
+				$scope.invalid = res.data.message;
+			} else {
+				$scope.description = res.data.description;
+				$scope.windspeed = res.data.windspeed;
+				$scope.city = res.data.city;
+				$scope.cTemp = res.data.cTemp;
+				$scope.fTemp = res.data.fTemp;
+				$scope.icon = res.data.icon;
+				$scope.humidity = res.data.humidity;
+				$scope.value1 = "true";
+				$scope.value2 = !$scope.value1;
+
+				switch($scope.description) {
+				case "Clear": 
+					$scope.myObj = {
+						"background":"url('../img/clear.jpg')",
+						"background-size":"cover"
+					};
+					break;
+				case "Clouds": 
+					$scope.myObj = {
+						"background":"url('../img/clouds.jpg')",
+						"background-size":"cover"
+					};
+					break;
+				case "Haze":
+					$scope.myObj = {
+						"background":"url('../img/haze.jpg')",
+						"background-size":"cover"
+					};
+					break;
+				case "Rain": 
+					$scope.myObj = {
+						"background":"url('../img/rain.jpg')",
+						"background-size":"cover"
+					};
+					break;
+				case "Snow": 
+					$scope.myObj = {
+						"background":"url('../img/snow.jpg')",
+						"background-size":"cover"
+					};
+					break;
+				case "Extreme": 
+					$scope.myObj = {
+						"background":"url('../img/extreme.jpg')",
+						"background-size":"cover"
+					};
+					break;
+				default: 
+					$scope.myObj = {
+						"background":"url('../img/background.jpg')",
+						"background-size":"cover"
+					};
+					break;
+				};
+				
+
+			};
+	
+		});
+		});
+	};
+});
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var request = require("request");
+
+var app = express();
+
+app.get("/weather", function(req, res) {
+	var lat = req.query.lat;
+	var lon = req.query.lon;
+	var appid = req.query.appid;
+	var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + appid;
+	
+	// debug
+	// console.log("Server URL: " + url);
+
+	request.get(url, function(err, response, body) {
+
+		body = JSON.parse(body);
+
+		if (err) {
+			res.send("Connection to OpenWeatherMap Failed");
+		} else if (body.cod == 401) {
+			res.send(body);
+			console.log(body);
+		} else {
+			var weather = {};
+			weather.cod = body.cod;
+			weather.city = body.name;
+			weather.description = body.weather[0].main;
+			weather.cTemp = Math.floor(body.main.temp - 273.15) + " °C";
+			weather.fTemp = Math.floor((body.main.temp - 273.15) * 9 / 5 + 32) + " °F";
+			weather.windspeed = body.wind.speed.toFixed(1) + " m/s";
+			weather.humidity = body.main.humidity + "%";
+			weather.icon = "http://openweathermap.org/img/w/" + body.weather[0].icon + ".png";
+			
+			res.send(weather);
+		}
+	});
+});
+		
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000, function() {
+	console.log("Server is listening on PORT 3000");
+});
